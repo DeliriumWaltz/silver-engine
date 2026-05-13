4ef51a14cca8cc19ea44fb466e953033a5c5151a
DeliriumWaltz <jampkan@pm.me>
2026-05-13 21:04:27 +1000

    refactor: add graceful shutdown to main server

--- cmd/chat-server/main.go
+++ cmd/chat-server/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
-	"fmt"
+	"context"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/DeliriumWaltz/silver-engine/internal/api"
 	"github.com/DeliriumWaltz/silver-engine/internal/chat"
@@ -27,9 +30,22 @@ func main() {
 	handler.RegisterRoutes(mux)
 	handler.RegisterWSRoute(mux, wsHub)
 
-	addr := fmt.Sprintf(":%s", port)
+	addr := ":" + port
+	srv := &http.Server{Addr: addr, Handler: mux}
+
+	// Graceful shutdown on SIGINT/SIGTERM
+	go func() {
+		sigCh := make(chan os.Signal, 1)
+		signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
+		<-sigCh
+		log.Println("Shutting down...")
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+		srv.Shutdown(ctx)
+	}()
+
 	log.Printf("Chat server starting on %s", addr)
-	if err := http.ListenAndServe(addr, mux); err != nil {
+	if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
 		log.Fatalf("server failed: %v", err)
 	}
 }
